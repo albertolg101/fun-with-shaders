a03447875b70cd2f7db4144e83851f95ca7b0594
Alberto Leyva Guerra <albertoleyvaguerra@gmail.com>
2025-08-07 04:32:22 +0200

    feat: adds deploy capabilites

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import glsl from "vite-plugin-glsl";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "https://albertolg101.github.io/fun-with-shaders",
   plugins: [glsl()],
 });
